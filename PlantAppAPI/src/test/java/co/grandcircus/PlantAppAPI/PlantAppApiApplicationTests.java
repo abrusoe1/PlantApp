3c8528f81f5e01007fb86d70cc1f307db2eb525d
thishastobenoah <thishastobenoah@gmail.com>
2023-04-12 20:40:27 -0400

    added back end

---
+++ PlantAppAPI/src/test/java/co/grandcircus/PlantAppAPI/PlantAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.PlantAppAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
