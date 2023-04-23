0769c5df7c7cc6790c821f41ec8d44dd0b3465c4
Andrew <abrusoe1@gmail.com>
2023-04-22 20:56:51 -0400

    calendar component and an add to personal function within plant search

--- PlantApp/src/app/personal-plant.ts
+++ PlantApp/src/app/personal-plant.ts
@@ -1,10 +1,11 @@
 export interface PersonalPlant {
     id:              number;
-    nickname:        string;
+    nickname?:        string;
     common_name:     string;
-    scientific_name: string;  //May need to be array to match other model
-    other_name:      string;  //May need to be array to match other model
+    scientific_name: string[];  //May need to be array to match other model
+    other_name:      string[];  //May need to be array to match other model
     watering:        Watering;
+    day?:             number;
 }
 
 export enum Watering {
